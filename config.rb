413b7b6fffc0051e1fa19d90df419a32bcd5b919
Nguyen Tien Dung <tiendung@CPDT-SERVER.(none)>
2010-04-09 10:45:12 +0700

    Init site

---
+++ config.rb
@@ -0,0 +1,13 @@
+require 'compass-colors'
+require 'fancy-buttons'
+
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "src/stypesheets"
+partial_dir ="src/partials"
+images_dir = "images"
+javascripts_dir = "javascripts"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
