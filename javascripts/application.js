42ea8f059f6066a6fb17ead77ddc20fb5f814708
Nguyen Tien Dung <tiendung@CPDT-SERVER.(none)>
2010-04-14 11:10:31 +0700

    Refine #!

--- javascripts/application.js
+++ javascripts/application.js
@@ -2,4 +2,6 @@ $(function () {
   // $("#container").corner("10px");
   // $("#logo").corner("top 9px");
   // $("#container").boxShadow(10, 10, 5, "#888");
+
+  $("#menu li.selected").corner("top right 6px");
 });
