ec2c5182e80616c4bba1bf4a95957047714d3e1f
Alex Nguyen <dungtn@gmail.com>
2010-04-15 14:41:54 +0700

    bevel tabs

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,7 +1,5 @@
 $(function () {
-  $("#container").corner("10px");
-  $("#logo").corner("top 9px");
-  $("#container").boxShadow(10, 10, 5, "#888");
-  
-  $("#menu").tabs();  
+  // $("#container").corner("10px");
+  // $("#logo").corner("top 9px");
+  $("#menu .selected a").corner("bevel tr 7px");
 });
